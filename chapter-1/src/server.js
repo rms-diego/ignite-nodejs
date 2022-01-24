544f2413c7f56e70a23dbe1eac5178ff4854409c
Diego Brito <diegorbrito9@gmail.com>
2022-01-24 10:31:35 -0300

    [CHAPTER 1] fim da aula 'utilizando os metodos http'

--- chapter-1/src/server.js
+++ chapter-1/src/server.js
@@ -1,8 +1,36 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3333;
+const PORT = 3333;  
 
-app.get('/', (request, response) => response.json({massage: 'Hello World !'}));
+app.get('/course', (request, response) => {
+  return response.json([
+    {curso1: 'curso1'},
+    {curso2: 'curso2'},
+    {curso3: 'curso3'},
+    {curso4: 'curso4'},
+  ])
+});
 
-app.listen(PORT, () => console.log(`🚀 Server up !🚀\nhttp://localhost:${PORT}`))
+
+app.post('/course', (request, response) => {
+  return response.json([
+    {curso1: 'curso1'},
+    {curso2: 'curso2'},
+    {curso3: 'curso3'},
+    {curso4: 'curso4'},
+  ])
+});
+
+app.put('/course/:id', (request, response) => {
+  
+  return response.json([
+    {curso1: 'curso1'},
+    {curso2: 'curso2'},
+    {curso3: 'curso3'},
+    {curso4: 'curso4'},
+  ])
+});
+
+
+app.listen(PORT, () => console.log(`🚀 Server up !🚀\nhttp://localhost:${PORT}`));
