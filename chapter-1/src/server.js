066ffc8b26d36117a4b2f2a81e1c4580d5340c1a
Diego Brito <diegorbrito9@gmail.com>
2022-02-12 17:15:19 -0300

    [chapter 1] fim da aula 'utilizando os metodos http'

--- chapter-1/src/server.js
+++ chapter-1/src/server.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.status(200).json({ message: "Hello world!" });
+app.get("/courses", (request, response) => {
+  return response.status(200).json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.status(200).json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/course/:id", (request, response) => {
+  return response.status(200).json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.status(200).json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.status(200).json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 app.listen(3333, () => console.log("server up 🚀\nhttp://localhost:%s", 3333));
