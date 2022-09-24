fcbac22d43ce4021cdd2aae348fda7fbdd3ffd0e
Diego Brito <diegorbrito9@gmail.com>
2022-09-24 16:52:14 -0300

    [ignite] [nodejs] [primeito projeto com node js] fim da aula 'cadastro de conta'

--- chapter-1/primeiro-projeto-com-nodejs/src/index.js
+++ chapter-1/primeiro-projeto-com-nodejs/src/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const { randomUUID: uuid } = require("crypto");
 
 const app = express();
 app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { name, cpf } = req.body;
+
+  const accountExists = customers.some((account) => account.cpf === cpf);
+
+  if (accountExists) return res.json({ message: "This account exists" });
+
+  customers.push({ id: uuid(), name, cpf, statement: [] });
+  return res.status(201).end();
+});
 
 app.listen(3333, () => console.log(`Server up 🚀\nhttp://localhost:3333`));
