54a99536ae9a4719b4f2658360abfa3db4be93ef
Diego Brito <diegorbrito9@gmail.com>
2022-09-24 16:27:17 -0300

    [ignite] [nodejs] [fundamentos do nodejs] [configurando o projeto]  fim da aula 'tipos de parametros'

--- chapter-1/fundamentos-nodejs/src/index.js
+++ chapter-1/fundamentos-nodejs/src/index.js
@@ -1,17 +1,29 @@
-const app = require("express")();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
 
 const courses = ["curso 1", "curso 2", "curso 3", "curso 4"];
 
 app.get("/courses", (req, res) => {
+  const { teste } = req.query;
+
+  console.log(teste);
   return res.json(courses);
 });
 
 app.post("/courses", (req, res) => {
-  courses.push(`curso ${courses.length + 1}`);
+  const { name } = req.body;
+  courses.push(name);
+
   return res.json(courses);
 });
 
 app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
   return res.json(["curso 6", "curso 2", "curso 3", "curso 4"]);
 });
 
