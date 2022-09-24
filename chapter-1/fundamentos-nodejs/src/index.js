54096f43106a83ddebf3cd69fc4e592e4b9b6120
Diego Brito <diegorbrito9@gmail.com>
2022-09-24 16:07:58 -0300

    [ignite] [nodejs] [fundamentos do nodejs] [configurando o projeto]  fim da aula 'utilizando os metodos http'

--- chapter-1/fundamentos-nodejs/src/index.js
+++ chapter-1/fundamentos-nodejs/src/index.js
@@ -1,7 +1,22 @@
 const app = require("express")();
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello world ignite - Fundamentos NodeJs" });
+const courses = ["curso 1", "curso 2", "curso 3", "curso 4"];
+
+app.get("/courses", (req, res) => {
+  return res.json(courses);
+});
+
+app.post("/courses", (req, res) => {
+  courses.push(`curso ${courses.length + 1}`);
+  return res.json(courses);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["curso 6", "curso 2", "curso 3", "curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["curso 2", "curso 3", "curso 4"]);
 });
 
 app.listen(3333, () =>
