07289a724e897ab0d19ac8e35fc3f440e0fce054
Diego Brito <diegorbrito9@gmail.com>
2022-09-25 15:12:10 -0300

    [ignite] [nodejs] [chapter 2] [criando a api com nodejs] fim da aula 'criando categoria'

--- chapter-2/criando-api-com-nodejs/src/server.ts
+++ chapter-2/criando-api-com-nodejs/src/server.ts
@@ -1,15 +1,12 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.status(200).send("olá mundo !"));
-
-app.post("/course", (req, res) => {
-  const { name } = req.body;
-  return res.status(200).json({ name });
-});
+// routes
+app.use(categoriesRoutes);
 
 app.listen(3333, () =>
   console.log("Server is running!\nhttp://localhost:3333")
