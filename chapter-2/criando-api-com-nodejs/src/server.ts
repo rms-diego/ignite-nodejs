a9af6ff520cda1269767420d3714db406b426e13
Diego Brito <diegorbrito9@gmail.com>
2022-09-25 15:00:25 -0300

    [ignite] [nodejs] [chapter 2] [criando a api com nodejs] fim da aula 'debugando a aplicação'

--- chapter-2/criando-api-com-nodejs/src/server.ts
+++ chapter-2/criando-api-com-nodejs/src/server.ts
@@ -2,7 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => res.status(200).send("olá mundo !"));
+
+app.post("/course", (req, res) => {
+  const { name } = req.body;
+  return res.status(200).json({ name });
+});
 
 app.listen(3333, () =>
   console.log("Server is running!\nhttp://localhost:3333")
