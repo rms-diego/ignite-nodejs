5d24c01c576f19fdcdd4c3dac2cecb589385af75
Diego Brito <diegorbrito9@gmail.com>
2022-10-31 21:07:18 -0300

    [ignite] [nodejs] [chapter 2] [criando a api com nodejs] fim da aula 'configurando ts-node-dev'

--- chapter-2/criando-a-api-com-nodejs/src/server.ts
+++ chapter-2/criando-a-api-com-nodejs/src/server.ts
@@ -1,5 +1,10 @@
+import { Request, Response } from "express";
 import app from "./app";
 
 const PORT = 5000;
+
+app.get("/", (req: Request, res: Response) =>
+  res.status(200).json({ message: "Hello world !" })
+);
 
 app.listen(PORT, () => console.log(`Server up\nPORT: ${PORT}`));
