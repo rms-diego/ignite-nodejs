cdd6b0f12ced21afa1709ebd7234ba36da72fc1b
Diego Ramos <diegorbrito9@gmail.com>
2023-02-13 15:58:55 -0300

    [ignite] [nodejs] [chapter 1] [configurando o projeto] fim da aula 'adicionando o nodemon na aplicação'

--- chapter-1/src/index.js
+++ chapter-1/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => response.json({ message: "hello world" }));
+app.get("/", (request, response) =>
+  response.json({ message: "hello world - ignite nodejs" })
+);
 
 app.listen(3333, () => console.log(`Server up on port ${3333}`));
