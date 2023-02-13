7ecc186e03c90abeec6dfc009b4bbbb0d0d1896c
Diego Ramos <diegorbrito9@gmail.com>
2023-02-13 17:08:19 -0300

    [ignite] [nodejs] [chapter 2] [primeiro projeto com nodejs] fim da aula 'validando o cpf existente'

--- chapter-2/src/routes/index.js
+++ chapter-2/src/routes/index.js
@@ -8,6 +8,12 @@ const accounts = [];
 routes.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
+  const customerAlreadyExists = accounts.some((account) => account.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ message: "customer already exists" });
+  }
+
   const accountCreated = {
     id: uuid(),
     name,
