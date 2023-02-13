272970fda46e7f42da9c4c2efb5a205967fe7f72
Diego Ramos <diegorbrito9@gmail.com>
2023-02-13 16:08:44 -0300

    [ignite] [nodejs] [chapter 1] [configurando o projeto] fim da aula 'utilizando os metodos http'

--- chapter-1/src/index.js
+++ chapter-1/src/index.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) =>
-  response.json({ message: "hello world - ignite nodejs" })
-);
+const courses = ["course 1", "course 2", "course 3", "course 4", "course 5"];
+app.get("/course", (request, response) => {
+  return response.json(courses);
+});
+
+app.post("/course", (request, response) => {
+  courses.push("course 6");
+
+  return response.json(courses);
+});
+
+app.put("/course/:id", (request, response) => {
+  return response.json([
+    "course 5",
+    "course 1",
+    "course 4",
+    "course 2",
+    "course 3",
+  ]);
+});
+
+app.delete("/course/:id", (request, response) => {
+  return response.json(["course 1", "course 2", "course 3", "course 4"]);
+});
 
 app.listen(3333, () => console.log(`Server up on port ${3333}`));
