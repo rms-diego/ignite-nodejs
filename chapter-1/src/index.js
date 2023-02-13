e00d9cd1f3fbdedfbbed4cf47e7403564d2233cb
Diego Ramos <diegorbrito9@gmail.com>
2023-02-13 15:54:32 -0300

    [ignite] [nodejs] [chapter 1] [configurando o projeto] fim da aula 'criando a estrutura do projeto'

---
+++ chapter-1/src/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => response.json({ message: "hello world" }));
+
+app.listen(3333, () => console.log(`Server up on port ${3333}`));
