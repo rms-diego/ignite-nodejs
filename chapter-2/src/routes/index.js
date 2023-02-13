98836324af417a0b855ed0f7e9f34a07cb8cf285
Diego Ramos <diegorbrito9@gmail.com>
2023-02-13 18:02:46 -0300

    [ignite] [nodejs] [chapter 2] [primeiro projeto com nodejs] fim da aula 'criando deposito na conta'

--- chapter-2/src/routes/index.js
+++ chapter-2/src/routes/index.js
@@ -6,7 +6,7 @@ const routes = Router();
 const accounts = [];
 
 const verifyExistentCpf = (request, response, next) => {
-  const { cpf } = request.body;
+  const { cpf } = request.headers;
 
   const userFound = accounts.find((account) => account.cpf === cpf);
 
@@ -44,6 +44,21 @@ routes.get("/statement", verifyExistentCpf, (request, response) => {
   const { customer } = request;
 
   return response.status(200).json(customer.statement);
+});
+
+routes.post("/deposit", verifyExistentCpf, (request, response) => {
+  const { customer } = request;
+  const { description, amount } = request.body;
+
+  const newOperation = {
+    amount,
+    description,
+    createdAt: new Date().toLocaleString(),
+    type: "credit",
+  };
+
+  customer.statement.push(newOperation);
+  return response.status(201).json(customer.statement);
 });
 
 module.exports = routes;
