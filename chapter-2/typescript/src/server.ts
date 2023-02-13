32f55b0bbbae236d2775e964b50c4184b5766d5a
Diego Ramos <diegorbrito9@gmail.com>
2023-02-13 13:46:29 -0300

    [ignite] [nodejs] recomeçando o curso

--- chapter-2/typescript/src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createCourse } from "./routes";
-
-const app = express();
-
-app.get("/", createCourse);
-
-app.listen(3333, () => console.log("Server up 🚀\nhttp://localhost:3333"));
