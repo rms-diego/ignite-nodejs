b5e796995b2461cee849d64326b62216513e10a1
Diego Ramos <diegorbrito9@gmail.com>
2023-03-08 14:11:44 -0300

    [ignite] [nodejs] [Projeto 1] [fundamentos do node.js] [Estrutura da aplicação] fim da aula 'Salvando usuários em memória (headers)'

--- projeto-1/src/server.js
+++ projeto-1/src/server.js
@@ -1,14 +1,26 @@
 import http from "node:http";
 
+import { randomUUID } from "node:crypto";
+
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url } = request;
 
   if (method === "GET" && url === "/users") {
-    return response.end("Listagem de usuários");
+    return response
+      .setHeader("content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return response.end("Criação de usuários");
+    users.push({
+      id: randomUUID(),
+      name: "Diego Ramos",
+      email: "diegoramos@email.com",
+    });
+
+    return response.end();
   }
 
   return response.end("Hello world!");
