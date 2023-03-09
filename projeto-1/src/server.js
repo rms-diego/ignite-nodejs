a142e7d8025852638b341d80556acc5e4f9972a8
Diego Ramos <diegorbrito9@gmail.com>
2023-03-09 13:48:28 -0300

    [ignite] [nodejs] [Projeto 1] [fundamentos do node.js] [Estrutura da aplicação] fim da aula 'corpo da requisição em JSON (stream & buffers)'

--- projeto-1/src/server.js
+++ projeto-1/src/server.js
@@ -4,20 +4,33 @@ import { randomUUID } from "node:crypto";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
-
   response.setHeader("content-type", "application/json");
+
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    request.body = null;
+  }
 
   if (method === "GET" && url === "/users") {
     return response.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = request.body;
+
     users.push({
       id: randomUUID(),
-      name: "Diego Ramos",
-      email: "diegoramos@email.com",
+      name,
+      email,
     });
 
     return response.writeHead(201).end();
