3a8cf43f0dc50ac2d7528da25ae6a675990298eb
Diego Ramos <diegorbrito9@gmail.com>
2023-03-18 21:46:57 -0300

    [ignite] [nodejs] [Projeto 2] [criando apis RESTfull com nodejs] [testes automatizados] fim da aula 'Categorizando os testes'

--- projeto-2/tests/transactions.test.ts
+++ projeto-2/tests/transactions.test.ts
@@ -1,21 +1,23 @@
-import { test, expect, beforeAll, afterAll } from "vitest";
+import { test, expect, beforeAll, afterAll, describe } from "vitest";
 import request from "supertest";
 
 import { app } from "../src/app";
 
-beforeAll(async () => app.ready());
+describe("Tests transactions routes", () => {
+  beforeAll(async () => app.ready());
 
-afterAll(async () => app.close());
+  afterAll(async () => app.close());
 
-test("[POST] should be able create a new transaction", async () => {
-  const transactionBody = {
-    title: "Um titulo qualquer",
-    amount: 10000,
-    type: "credit",
-  };
+  test("[POST] It should be able user create a new transaction", async () => {
+    const transactionBody = {
+      title: "Um titulo qualquer",
+      amount: 10000,
+      type: "credit",
+    };
 
-  await request(app.server)
-    .post("/transactions/create")
-    .send(transactionBody)
-    .expect(201);
+    await request(app.server)
+      .post("/transactions/create")
+      .send(transactionBody)
+      .expect(201);
+  });
 });
