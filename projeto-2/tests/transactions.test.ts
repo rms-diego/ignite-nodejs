78a5c441988e4f6319f8adc073c75cf4d687e42f
Diego Ramos <diegorbrito9@gmail.com>
2023-03-20 15:44:32 -0300

    [ignite] [nodejs] [Projeto 2] [criando apis RESTfull com nodejs] [testes automatizados] fim da aula 'testando listagem de transações'

--- projeto-2/tests/transactions.test.ts
+++ projeto-2/tests/transactions.test.ts
@@ -4,6 +4,8 @@ import request from "supertest";
 import { app } from "../src/app";
 
 describe("Tests transactions routes", () => {
+  const server = app.server;
+
   beforeAll(async () => app.ready());
 
   afterAll(async () => app.close());
@@ -15,9 +17,35 @@ describe("Tests transactions routes", () => {
       type: "credit",
     };
 
-    await request(app.server)
+    await request(server)
       .post("/transactions/create")
       .send(transactionBody)
       .expect(201);
+  });
+
+  test("[GET] It should be able get all transactions", async () => {
+    const transactionBody = {
+      title: "Um titulo qualquer",
+      amount: 10000,
+      type: "credit",
+    };
+
+    const response = await request(server)
+      .post("/transactions/create")
+      .send(transactionBody)
+      .expect(201);
+
+    const cookie = response.get("Set-Cookie");
+
+    const {
+      body: { transactions },
+    } = await request(server)
+      .get("/transactions/findMany")
+      .set("Cookie", cookie)
+      .expect(200);
+
+    expect(transactions).toEqual([
+      expect.objectContaining({ title: "Um titulo qualquer", amount: 10000 }),
+    ]);
   });
 });
