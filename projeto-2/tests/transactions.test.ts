4b9e597282a77f394708168c56851fedabd40a34
Diego Ramos <diegorbrito9@gmail.com>
2023-03-21 18:43:19 -0300

    [ignite] [nodejs] [Projeto 2] [criando apis RESTfull com nodejs] [testes automatizados] fim da aula 'finalizando testes da aplicação'

--- projeto-2/tests/transactions.test.ts
+++ projeto-2/tests/transactions.test.ts
@@ -26,7 +26,7 @@ describe("Tests transactions routes", () => {
     execSync(resetDb);
   });
 
-  test("[POST] It should be able user create a new transaction", async () => {
+  test("[POST] [/transactions/create] It should be able user create a new transaction", async () => {
     const transactionBody = {
       title: "Um titulo qualquer",
       amount: 10000,
@@ -39,7 +39,7 @@ describe("Tests transactions routes", () => {
       .expect(201);
   });
 
-  test("[GET] It should be able get all transactions", async () => {
+  test("[GET] [/transactions/findMany] It should be able get all transactions", async () => {
     const transactionBody = {
       title: "Um titulo qualquer",
       amount: 10000,
@@ -63,5 +63,76 @@ describe("Tests transactions routes", () => {
     expect(transactions).toEqual([
       expect.objectContaining({ title: "Um titulo qualquer", amount: 10000 }),
     ]);
+  });
+
+  test("[GET] [/transactions/findOne/:transactionId] It should be able get specific transactions", async () => {
+    const transactionBody = {
+      title: "Um titulo qualquer",
+      amount: 10000,
+      type: "credit",
+    };
+
+    const response = await request(server)
+      .post("/transactions/create")
+      .send(transactionBody)
+      .expect(201);
+
+    const cookie = response.get("Set-Cookie");
+
+    const {
+      body: { transactions },
+    } = await request(server)
+      .get("/transactions/findMany")
+      .set("Cookie", cookie)
+      .expect(200);
+
+    const transactionId = transactions[0].id;
+
+    const {
+      body: { transaction },
+    } = await request(server)
+      .get(`/transactions/findOne/${transactionId}`)
+      .set("Cookie", cookie)
+      .expect(200);
+
+    expect(transaction).toEqual(
+      expect.objectContaining({ title: "Um titulo qualquer", amount: 10000 })
+    );
+  });
+
+  test("[GET] [/transactions/summary] It should be able get the summary", async () => {
+    const creditTransactionBody = {
+      title: "Um titulo qualquer",
+      amount: 10000,
+      type: "credit",
+    };
+
+    const debitTransactionBody = {
+      title: "Um titulo qualquer",
+      amount: 5000,
+      type: "debit",
+    };
+
+    const creditTransactionResponse = await request(server)
+      .post("/transactions/create")
+      .send(creditTransactionBody)
+      .expect(201);
+
+    const cookie = creditTransactionResponse.get("Set-Cookie");
+
+    await request(server)
+      .post("/transactions/create")
+      .send(debitTransactionBody)
+      .set("Cookie", cookie)
+      .expect(201);
+
+    const {
+      body: { summary },
+    } = await request(server)
+      .get("/transactions/summary")
+      .set("Cookie", cookie)
+      .expect(200);
+
+    expect(summary).toEqual({ amount: 5000 });
   });
 });
