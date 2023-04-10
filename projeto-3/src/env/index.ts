828fc5de3b60f415832968fe6b67a2ed2c822d1e
Diego Ramos <diegorbrito9@gmail.com>
2023-04-10 16:32:18 -0300

    [ignite] [nodejs] [Projeto 3] [API node.js com solid] [Integração com o prisma orm] fim da aula 'Relacionamentos entre tabelas'

--- projeto-3/src/env/index.ts
+++ projeto-3/src/env/index.ts
@@ -5,6 +5,7 @@ import { z as zod } from "zod";
 const envSchema = zod.object({
   NODE_ENV: zod.enum(["dev", "test", "production"]).default("dev"),
   PORT: zod.coerce.number().default(3333),
+  DATABASE_URL: zod.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
