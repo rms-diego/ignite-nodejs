bc3890e17f49130895698316fa4626e4f7c8ddeb
Diego Ramos <diegorbrito9@gmail.com>
2023-04-10 15:17:43 -0300

    [ignite] [nodejs] [Projeto 3] [API node.js com solid] [estrutura do projeto] fim da aula 'Carregando variáveis de ambiente'

--- projeto-3/src/server.ts
+++ projeto-3/src/server.ts
@@ -1,6 +1,7 @@
 import { app } from "./app";
+import { env } from "./env";
 
-app.listen({ host: "0.0.0.0", port: 3000 }, (error, address) => {
+app.listen({ host: "0.0.0.0", port: env.PORT }, (error, address) => {
   if (error) throw new Error(error.message);
 
   console.log(`Server is running\nlink:${address}`);
