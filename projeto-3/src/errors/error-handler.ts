ca2ace163726ebae529874f1b5485da204b2e671
Diego Ramos <diegorbrito9@gmail.com>
2023-04-12 15:22:04 -0300

    [ignite] [nodejs] [Projeto 3] [API node.js com solid] [Caso de uso e design patterns] fim da aula 'Handler de erros globais'

--- projeto-3/src/errors/error-handler.ts
+++ projeto-3/src/errors/error-handler.ts
@@ -1,5 +1,9 @@
-import { Exception } from "@/errors/Exception";
 import { FastifyError, FastifyReply, FastifyRequest } from "fastify";
+
+import { Exception } from "@/errors/Exception";
+import { ZodError } from "zod";
+
+import { env } from "@/env";
 
 export const errorHandler = async (
   error: FastifyError,
@@ -8,6 +12,18 @@ export const errorHandler = async (
 ) => {
   if (error instanceof Exception) {
     return response.status(error.statusCode).send({ error: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    const errorIssues = error.format();
+
+    return response
+      .status(400)
+      .send({ error: "validations error", issues: errorIssues });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
   }
 
   return response.status(500).send({ error: error.message });
